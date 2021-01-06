ab253a0e8328fb889b4920e355750de482dca0ee
sayyedkausar <60963919+sayyedkausar@users.noreply.github.com>
2021-01-06 14:52:26 +0530

    Add files via upload

---
+++ src/Table.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+class Table_had extends Component {
+    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Student</th>
+                        <th>Roll_no</th>
+                        <th>Class</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Kausar</td>
+                        <td>76</td>
+                        <td>syit</td>
+                    </tr>
+                    <tr>
+                        <td>saba</td>
+                        <td>82</td>
+                        <td>syit</td>
+                    </tr>
+                </tbody>
+            </table>
+        )
+    }
+}
+
+export default Table_had;
